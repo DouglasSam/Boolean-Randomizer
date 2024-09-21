122a86701bb4bcaa0446f52b27c4ade2ad3d9f8a
Sam <sdouglas2998@gmail.com>
2024-09-21 14:05:16 +1200

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    document.getElementById('chance1').addEventListener('input', function() {
+        const chance1 = Number(this.value);
+        const chance2 = 100 - chance1;
+        document.getElementById('chance2').value = chance2;
+    });
+
+    document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        const randomValue = Math.random(); // Generate a random value between 0 and 1
+        const value1 = document.getElementById('value1').value;
+        const value2 = document.getElementById('value2').value;
+        const chance1 = Number(document.getElementById('chance1').value)/100;
+        // const chance2 = Number(document.getElementById('chance2').value)/100;
+        let ret;
+        if (randomValue < chance1) {
+            ret = value1;
+        } else {
+            ret = value2;
+        }
+        document.getElementById('result').innerText = `Random Value: ${ret}`;
+    });
+};
